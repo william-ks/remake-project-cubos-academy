30492a2c2598fc477fc07bc25425e32b3682a179
william krisley <will.ks2101@gmail.com>
2023-08-02 15:27:53 -0300

    refact createUser for error handling

--- src/api/useCases/createUser/createUserController.ts
+++ src/api/useCases/createUser/createUserController.ts
@@ -9,14 +9,8 @@ export class CreateUserUserController {
     async handle(req: Request, res: Response): Promise<Response> {
         const { name, email, password } = req.body;
 
-        try {
-            await this.createUserUseCase.execute({ name, email, password });
+        await this.createUserUseCase.execute({ name, email, password });
 
-            return res.status(201).send();
-        } catch (e) {
-            return res.status(400).json({
-                message: e.message || "Unexpected Error!"
-            })
-        }
+        return res.status(201).send();
     }
 }
