cd1a1140c99e875019703113b44bc71fef335867
william krisley <will.ks2101@gmail.com>
2023-08-08 12:38:38 -0300

    use case for update user

--- src/api/providers/implementations/JwtTokens.ts
+++ src/api/providers/implementations/JwtTokens.ts
@@ -3,10 +3,16 @@ import { IJwt, data } from '../IJwt';
 
 export class JwtTokens implements IJwt {
     sign(data: data): string {
-        const token = jwt.sign({ ...data.user }, process.env.JWT_PASS || 'senha', {
+        const token = jwt.sign({ ...data.user }, process.env.JWT_PASS || 'needValue', {
             expiresIn: data.expiresIn
         })
 
         return token
+    }
+
+    verify(token: string): Object | Error {
+        const data = jwt.verify(token, process.env.JWT_PASS || 'needValue');
+
+        return data
     }
 }
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,20 +4,30 @@ import { createUserController } from "./use-cases/createUser";
 import { createUserSchema } from "./use-cases/createUser/createUserSchema";
 import { AuthenticateUserSchema } from "./use-cases/authenticateUser/AuthenticateUserSchema";
 import { authenticateUserController } from "./use-cases/authenticateUser";
+import { authenticatedUserVerify } from "./middleware/authenticatedUserVerify";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    return res.status(200).send()
+router.post("/user", bodyValidation(createUserSchema), (req, res) => {
+  return createUserController.handle(req, res);
+});
+
+router.post(
+  "/authenticate",
+  bodyValidation(AuthenticateUserSchema),
+  (req, res) => {
+    return authenticateUserController.handle(req, res);
+  }
+);
+
+router.use(authenticatedUserVerify);
+
+router.put("/user/update", (req, res) => {
+  
 })
 
-router.post('/user', bodyValidation(createUserSchema), (req, res) => {
-    return createUserController.handle(req, res);
-})
-// gmail.com
-router.post('/authenticate', bodyValidation(AuthenticateUserSchema), (req, res) => {
-    return authenticateUserController.handle(req, res);
-})
+router.get("/", (req, res) => {
+  return res.status(200).send();
+});
 
 export { router };
-
