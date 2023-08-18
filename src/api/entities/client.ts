f4c72c84b93a26976b27fcb2cf81b2659a728895
william krisley <will.ks2101@gmail.com>
2023-08-18 03:29:30 -0300

    remove IClientAddress and add properties directly to the class Client

--- src/api/entities/client.ts
+++ src/api/entities/client.ts
@@ -1,20 +1,16 @@
 import { uuid } from "uuidv4";
 
-export interface IClientAddress {
-  zipCode?: string;
-  complement?: string;
-  neighborhood?: string;
-  city?: string;
-  state?: string;
-}
-
 export class Client {
   public readonly id?: string;
   public name: string;
   public email: string;
   public cpf: string;
   public phone: string;
-  public address?: IClientAddress;
+  public zipCode?: string;
+  public complement?: string;
+  public neighborhood?: string;
+  public city?: string;
+  public state?: string;
 
   constructor(props: Omit<Client, "id">, id?: string) {
     Object.assign(this, props);
